09baf2e3315fa836b5a2a7c4d33f461914952255
Hendi Santika <hendisantika@gmail.com>
2018-03-21 06:46:51 +0700

    #5 - Add Upload Controller  - Upload File Spring Boot

---
+++ SpringBootUploadFile/src/main/java/com/hendisantika/uploadfiles/controller/UploadController.java
@@ -0,0 +1,71 @@
+package com.hendisantika.uploadfiles.controller;
+
+import com.hendisantika.uploadfiles.storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-upload-files
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/03/18
+ * Time: 20.54
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+public class UploadController {
+
+    @Autowired
+    StorageService storageService;
+
+    List<String> files = new ArrayList<String>();
+
+    @PostMapping("/post")
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        String message = "";
+        try {
+            storageService.store(file);
+            files.add(file.getOriginalFilename());
+
+            message = "You successfully uploaded " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.OK).body(message);
+        } catch (Exception e) {
+            message = "FAIL to upload " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+        }
+    }
+
+    @GetMapping("/getallfiles")
+    public ResponseEntity<List<String>> getListFiles(Model model) {
+        List<String> fileNames = files
+                .stream().map(fileName -> MvcUriComponentsBuilder
+                        .fromMethodName(UploadController.class, "getFile", fileName).build().toString())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(fileNames);
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = storageService.loadFile(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
+}
